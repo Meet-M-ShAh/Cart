fe12583bd2432536f8ed11dcc8d237a3c317f5d8
Meet Shah <meet_outlook.in>
2020-12-04 15:18:42 +0530

    added increaseQuantity function

--- src/CartItem.js
+++ src/CartItem.js
@@ -10,6 +10,10 @@ class CartItem extends React.Component {
             img: ''
         }
     }
+
+    increaseQuantity = () => {
+        console.log('this.state', this.state);
+    }
     render () {
         const { price, title, qty } = this.state;
         return (
@@ -23,9 +27,22 @@ class CartItem extends React.Component {
                     <div style={ {color: '#777'} }>Qty: {qty}</div>
                     <div className="cart-item-actions">
                         {/*Buttons*/}
-                        <img alt="increase" className="action-icons" src="https://t4.ftcdn.net/jpg/01/07/62/07/240_F_107620769_UwNVSoXnKS4VNcOKoZjPohlEPn83oE38.jpg" />
-                        <img alt="decrease" className="action-icons" src="https://t3.ftcdn.net/jpg/03/73/49/86/240_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg" />
-                        <img alt="delete" className="action-icons" src="https://t4.ftcdn.net/jpg/00/98/26/11/240_F_98261175_Sv69O3rZsHApYkjAdrWbgQixYHwyZyOr.jpg" />
+                        <img 
+                            alt="increase"
+                            className="action-icons" 
+                            src="https://t4.ftcdn.net/jpg/01/07/62/07/240_F_107620769_UwNVSoXnKS4VNcOKoZjPohlEPn83oE38.jpg"
+                            onClick={this.increaseQuantity}
+                        />
+                        <img 
+                            alt="decrease" 
+                            className="action-icons" 
+                            src="https://t3.ftcdn.net/jpg/03/73/49/86/240_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg" 
+                        />
+                        <img 
+                            alt="delete" 
+                            className="action-icons" 
+                            src="https://t4.ftcdn.net/jpg/00/98/26/11/240_F_98261175_Sv69O3rZsHApYkjAdrWbgQixYHwyZyOr.jpg" 
+                        />
                     </div>
                 </div>
             </div>
